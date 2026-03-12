781ec51d03900dfca1fe5da3ce24812e54dc3775
Jason Rodriguez <jason.eli.rodriguez@gmail.com>
2026-03-12 01:01:43 -0700

    basic vim setup that works

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -7,18 +7,6 @@ require("catppuccin").setup(
 )
 vim.cmd.colorscheme("catppuccin")
 
-local ctp_feline = require("catppuccin.groups.integrations.feline")
-require("feline").setup(
-    {
-        components = ctp_feline.get(),
-        force_inactive = {
-            filetypes = {
-                "^neo%-tree$"
-            }
-        }
-    }
-)
-
 require("gitsigns").setup()
 
 vim.cmd([[hi NotificationInfo guifg=#FFFFFF guibg=#0F111A]])
